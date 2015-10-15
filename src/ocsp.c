eb5a6ac650fd6720467787d71798d80cc11129f6
Mark Steele <mark@control-alt-del.org>
2015-10-14 22:01:15 -0400

    packaging

--- src/ocsp.c
+++ src/ocsp.c
@@ -16,6 +16,7 @@ static gnutls_x509_crt_t load_cert(char *cert);
 
 static void _generate_request(gnutls_datum_t * rdata, gnutls_x509_crt_t cert,
                               gnutls_x509_crt_t issuer, gnutls_datum_t *nonce);
+
 static int _verify_response(gnutls_datum_t * data, gnutls_x509_crt_t cert,
                             gnutls_x509_crt_t signer, gnutls_datum_t *nonce);
 
@@ -42,7 +43,7 @@ int ocsp_check(char *cert_buf, char *issuer_buf, char *signer_buf)
 
   ret = gnutls_rnd(GNUTLS_RND_NONCE, nonce.data, nonce.size);
   if (ret < 0) {
-    exit(1);
+    goto cleanup;
   }
   ret = gnutls_x509_crt_get_authority_info_access(cert,
                                                   0,
@@ -50,28 +51,22 @@ int ocsp_check(char *cert_buf, char *issuer_buf, char *signer_buf)
                                                   &tmp,
                                                   NULL);
 
-  if (ret == GNUTLS_E_REQUESTED_DATA_NOT_AVAILABLE) {
-    exit(1);
-  }
-
-  if (ret < 0) {
-    exit(1);
+  if (ret != 0) {
+    goto cleanup;
   }
 
   ocsp_url = malloc(tmp.size + 1);
   memcpy(ocsp_url, tmp.data, tmp.size);
   ocsp_url[tmp.size] = 0;
-  gnutls_free(tmp.data);
 
   memset(&ud, 0, sizeof(ud));
+
   _generate_request(&req, cert, issuer, &nonce);
 
   curl_global_init(CURL_GLOBAL_ALL);
-
   handle = curl_easy_init();
   if (handle == NULL) {
-    // @TODO deinit stuff here...
-    exit(1);
+    goto cleanup;
   }
 
   headers =
@@ -84,51 +79,26 @@ int ocsp_check(char *cert_buf, char *issuer_buf, char *signer_buf)
   curl_easy_setopt(handle, CURLOPT_URL, ocsp_url);
   curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, get_data);
   curl_easy_setopt(handle, CURLOPT_WRITEDATA, &ud);
-
   ret = curl_easy_perform(handle);
-  if (ret != 0) {
-    // @TODO deinit stuff here
-    curl_easy_cleanup(handle);
-    exit(1);
-  }
 
-  curl_easy_cleanup(handle);
+  if (ret != 0) {
+    goto cleanup;
+  }
 
   v = _verify_response(&ud, cert, signer, &nonce);
 
+ cleanup:
+  free(ocsp_url);
+  curl_slist_free_all(headers);
+  curl_easy_cleanup(handle);
+  gnutls_free(tmp.data);
+  gnutls_free(ud.data);
   gnutls_x509_crt_deinit(cert);
   gnutls_x509_crt_deinit(issuer);
   gnutls_x509_crt_deinit(signer);
-  gnutls_global_deinit();
-
-  return v;
-}
-
-static void _generate_request(gnutls_datum_t * rdata, gnutls_x509_crt_t cert,
-                  gnutls_x509_crt_t issuer, gnutls_datum_t *nonce)
-{
-  gnutls_ocsp_req_t req;
-  int ret;
-
-  ret = gnutls_ocsp_req_init(&req);
-  if (ret < 0)
-    exit(1);
-
-  ret = gnutls_ocsp_req_add_cert(req, GNUTLS_DIG_SHA1, issuer, cert);
-  if (ret < 0)
-    exit(1);
-
-
-  ret = gnutls_ocsp_req_set_nonce(req, 0, nonce);
-  if (ret < 0)
-    exit(1);
-
-  ret = gnutls_ocsp_req_export(req, rdata);
-  if (ret != 0)
-    exit(1);
-
   gnutls_ocsp_req_deinit(req);
-  return;
+  gnutls_global_deinit();
+  return v;
 }
 
 static int
@@ -137,36 +107,37 @@ _verify_response(gnutls_datum_t * data, gnutls_x509_crt_t cert,
 {
   gnutls_ocsp_resp_t resp;
   int ret;
-  unsigned verify;
+  unsigned verify = -1;
   gnutls_datum_t rnonce;
 
   ret = gnutls_ocsp_resp_init(&resp);
   if (ret < 0) {
-    return 1;
+    goto cleanup;
   }
   ret = gnutls_ocsp_resp_import(resp, data);
   if (ret < 0) {
-    return 1;
+    goto cleanup;
   }
   ret = gnutls_ocsp_resp_check_crt(resp, 0, cert);
   if (ret < 0) {
-    return 1;
+    goto cleanup;
   }
 
   ret = gnutls_ocsp_resp_get_nonce(resp, NULL, &rnonce);
   if (ret < 0) {
-    return 1;
+    goto cleanup;
   }
 
   if (rnonce.size != nonce->size || memcmp(nonce->data, rnonce.data,nonce->size) != 0) {
-    return 1;
+    goto cleanup;
   }
 
   ret = gnutls_ocsp_resp_verify_direct(resp, signer, &verify, 0);
   if (ret < 0) {
-    return 1;
+    goto cleanup;
   }
 
+ cleanup:
   gnutls_free(rnonce.data);
   gnutls_ocsp_resp_deinit(resp);
   return verify;
@@ -180,12 +151,12 @@ size_t get_data(void *buffer, size_t size, size_t nmemb, void *userp)
 
   ud->data = realloc(ud->data, size + ud->size);
   if (ud->data == NULL) {
-    exit(1);
+    return NULL;
   }
 
   memcpy(&ud->data[ud->size], buffer, size);
   ud->size += size;
-
+  gnutls_free(ud.data);
   return size;
 }
 
@@ -204,9 +175,37 @@ static gnutls_x509_crt_t load_cert(char *cert)
   data.size = strlen(cert);
 
   ret = gnutls_x509_crt_import(crt, &data, GNUTLS_X509_FMT_PEM);
-  free(data.data);
   if (ret < 0) {
+    gnutls_free(data.data);
     return NULL;
   }
+  gnutls_free(data.data);
   return crt;
+}
+
+static void _generate_request(gnutls_datum_t * rdata, gnutls_x509_crt_t cert,
+                              gnutls_x509_crt_t issuer, gnutls_datum_t *nonce)
+{
+  gnutls_ocsp_req_t req;
+  int ret;
+
+  ret = gnutls_ocsp_req_init(&req);
+  if (ret < 0)
+    goto cleanup;
+
+  ret = gnutls_ocsp_req_add_cert(req, GNUTLS_DIG_SHA1, issuer, cert);
+  if (ret < 0)
+    goto cleanup;
+
+  ret = gnutls_ocsp_req_set_nonce(req, 0, nonce);
+  if (ret < 0)
+    goto cleanup;
+
+  ret = gnutls_ocsp_req_export(req, rdata);
+  if (ret != 0)
+    goto cleanup;
+
+ cleanup:
+  gnutls_ocsp_req_deinit(req);
+  return;
 }
